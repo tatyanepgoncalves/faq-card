a5ad09bc62a77ca17245baf8e9a71fcd24767a33
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-12-27 10:34:58 -0300

    :sparkles: feat: Página completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-
+import Accordion from "./components/Accordion";
+import ImageDesktop from "./assets/illustration-woman-online-desktop.svg";
+import ImageMobile from "./assets/illustration-woman-online-mobile.svg";
+import "./styles/App.scss";
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <main className="card">
+        <section className="image-box">
+          <img src={ImageDesktop} alt="Image Desktop" className="desktop" />
+          <img src={ImageMobile} alt="Image Mobile" className="mobile"/>
+        </section>
+          <form>
+            <h1>Faq</h1>
+            <Accordion />
+          </form>
+      </main>
+
+    </>
   )
 }
